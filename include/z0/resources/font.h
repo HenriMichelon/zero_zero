4ea59feab322e9bcf58168a1e568c855e7c5879e
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-05-14 18:31:29 +0200

    Merged Font & FontRenderer

--- include/z0/resources/font.h
+++ include/z0/resources/font.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include "z0/color.h"
 #include "z0/resources/resource.h"
-#include "z0/renderers/font_renderer.h"
 
 #include <map>
 
@@ -10,17 +10,18 @@ namespace z0 {
 
     class Font: public Resource {
     public:
-        explicit Font(const string& name): Resource{name} {};
+        explicit Font(const string& name);
+        ~Font();
 
         /*! Load a font.
-		Default font & size are architecture dependent
-		\param String&	: font name, if "" then the engine default font is loaded
-		\param String&	: engine name, if "" then the os/wm default engine is used
-		\param uint32_t	: size, if 0 then a default size is selected
-		\param bool	: bold
-		\param bool	: italic
-		\param bool	: underline
-	*/
+            Default font & size are architecture dependent
+            \param String&	: font name, if "" then the engine default font is loaded
+            \param String&	: engine name, if "" then the os/wm default engine is used
+            \param uint32_t	: size, if 0 then a default size is selected
+            \param bool	: bold
+            \param bool	: italic
+            \param bool	: underline
+	    */
         static shared_ptr<Font> create(const string& = "", const string& = "",
                                 uint32_t = 0, bool = false, bool = false, bool = false);
 
@@ -30,7 +31,11 @@ namespace z0 {
         void setTextColor(const Color&);
 
         /*! Return the maximum height (in pixels) for the font */
-        uint32_t getHeight() const { return fontRenderer.getHeight(); };
+        uint32_t getHeight() const { return height; };
+
+        /*! Return the maximum descent parameter
+            (number of pixels below the baseline) */
+        uint32_t getYMin() const { return ymin; }
 
         /*! Return the width (in pixels) for a character */
         uint32_t getWidth(wchar_t);
@@ -54,13 +59,37 @@ namespace z0 {
         void* render(wchar_t, int32_t&, int32_t&);
 
     private:
+        struct CachedCharacter {
+            /*! X Advance in pixels (space to the next character) */
+            int32_t	    xAdvance;
+            /*! left side bearing (space between left border of the char & the first char pixel) */
+            int32_t	    leftbearing;
+            /*! Ascent in pixel (number of pixel above the base line) */
+            int32_t	    ascent; // bboxtop
+            /*! Descent in pixel (number of pixel below the base line) */
+            int32_t	    descent; // yMin
+            /*! Height in pixels (normaly ascent+descent, but only for horizontal writting) */
+            uint32_t	height;
+            /*! Width in pixels*/
+            uint32_t	width;
+            /*! Rendered pixmap */
+            uint8_t*    bitmap;
+        };
+
         Color	                        textColor;
-        FontRenderer                    fontRenderer;
         map<wchar_t, CachedCharacter>   cache;
+        uint32_t                        height; // Maximum height of chars
+        uint32_t                        ymin; // font descent (number of lines under the font's baseline)
 
         CachedCharacter &getFromCache(wchar_t);
+        void render(CachedCharacter&, wchar_t);
+        bool openFont(const string&, uint32_t, bool, bool, bool);
 
-
+#ifdef _WIN32
+        MAT2	mat2;
+        HFONT	font{nullptr};
+        static FIXED   fixedFromDouble(double d);
+#endif
     };
 
 }
--- src/z0/resources/font.cpp
+++ src/z0/resources/font.cpp
@@ -2,19 +2,18 @@
 
 #include <locale>
 #include <codecvt>
+#include <cstring>
 
 namespace z0 {
 
     shared_ptr<Font> Font::create(const string& NAME, const string& ENGINE,
                          uint32_t SIZE, bool B, bool I, bool U) {
         auto font = make_shared<Font>(NAME);
-        if (font->fontRenderer.openFont(NAME, SIZE, B, I, U)) {
+        if (font->openFont(NAME, SIZE, B, I, U)) {
             return font;
         }
-        else {
-            die("Font::Create: Cannot open font ", NAME);
-            return nullptr;
-        }
+        die("Font::Create: Cannot open font ", NAME);
+        return nullptr;
     }
 
     uint32_t Font::getWidth(wchar_t CHAR) {
@@ -55,15 +54,151 @@ namespace z0 {
         return font.bitmap;
     }
 
-    CachedCharacter& Font::getFromCache(wchar_t CHAR) {
+    Font::CachedCharacter& Font::getFromCache(wchar_t CHAR) {
         if (cache.contains(CHAR)) {
             return cache[CHAR];
         } else {
             auto& cchar = cache[CHAR];
-            fontRenderer.render(cchar, CHAR);
+            render(cchar, CHAR);
             return cchar;
         }
     }
+
+#ifdef _WIN32
+
+    FIXED Font::fixedFromDouble(double d) {
+        long l = (long) (d * 65536L);
+        return *(FIXED *)&l;
+    }
+
+    Font::Font(const string& name): Resource{name} {
+        mat2.eM11 = fixedFromDouble(1.0);
+        mat2.eM12 = fixedFromDouble(0.0);
+        mat2.eM21 = fixedFromDouble(0.0);
+        mat2.eM22 = fixedFromDouble(1.0);
+    }
+
+Font::~Font() {
+        DeleteObject(font);
+    }
+
+    bool Font::openFont(const string &NAME, const uint32_t SIZE, const bool B, const bool I, const bool U) {
+        if (font != nullptr) { DeleteObject(font); }
+
+        string name;
+        if (NAME.empty()) {
+            name = "Verdana";
+        }
+        else {
+            name = NAME;
+        }
+
+        uint32_t size = SIZE;
+        if (!size) { size = 11; }
+
+        int weight = FW_NORMAL;
+        if (B) { weight = FW_BOLD; }
+
+        HDC dc = GetDC(nullptr);
+        if (!dc) return false;
+        font = CreateFont(-size,
+                          0,
+                          0,
+                          0,
+                          weight,
+                          I,
+                          U,
+                          FALSE,
+                          DEFAULT_CHARSET,
+                          OUT_DEFAULT_PRECIS,
+                          CLIP_DEFAULT_PRECIS,
+                          DEFAULT_QUALITY,
+                          DEFAULT_PITCH | FF_DONTCARE,
+                          name.c_str());
+        if (SelectObject(dc, font) == nullptr) {
+            die("FontRenderer::CreateFont error");
+            ReleaseDC(nullptr, dc);
+            return false;
+        }
+        TEXTMETRIC textmetric;
+        GetTextMetrics(dc, &textmetric);
+        height = textmetric.tmAscent + textmetric.tmDescent;
+        ymin = textmetric.tmDescent;
+        ReleaseDC(nullptr, dc);
+        return font != nullptr;
+    }
+
+    void Font::render(CachedCharacter &car, wchar_t CHAR) {
+        GLYPHMETRICS gm;
+
+        HDC dc = GetDC(nullptr);
+        if (!dc) return;
+        if (SelectObject(dc, font) == nullptr)	{
+            die("FontRenderer::render(): SelectObject() error\n");
+            ReleaseDC(nullptr, dc);
+            return;
+        }
+
+        UINT dchar = CHAR;
+        uint32_t size = GetGlyphOutlineA(dc,
+                                         dchar,
+                                         GGO_BITMAP, // + GGO_GLYPH_INDEX,
+                                         &gm,
+                                         0,
+                                         nullptr,
+                                         &mat2);
+        if (size == GDI_ERROR)	{
+            size = GetGlyphOutline(dc,
+                                   20,
+                                   GGO_BITMAP,
+                                   &gm,
+                                   0,
+                                   nullptr,
+                                   &mat2);
+            if (size == GDI_ERROR)	{
+                ReleaseDC(nullptr, dc);
+                return;
+            }
+        }
+
+        car.height = gm.gmBlackBoxY;
+        if (size)	{
+            if ((size*4) > (gm.gmBlackBoxX*gm.gmBlackBoxY))
+                car.width = gm.gmBlackBoxX+(size*4 - gm.gmBlackBoxX*gm.gmBlackBoxY)/gm.gmBlackBoxY;
+            else
+                car.width = gm.gmBlackBoxX-(gm.gmBlackBoxX*gm.gmBlackBoxY - size*4)/gm.gmBlackBoxY;
+
+            car.bitmap = new uint8_t[car.width*car.height];
+            auto *bitmap = new uint8_t[size];
+            GetGlyphOutline(dc, dchar, GGO_BITMAP, &gm, size, bitmap, &mat2);
+            std::memset(car.bitmap, 0, car.width*car.height);
+
+            uint32_t idx = 0;
+            for (uint32_t y=0; y<car.height; y++) {
+                for (uint32_t x=0; x<(car.width/8); x++) {
+                    for (uint8_t i=0; i<8; i++) {
+                        car.bitmap[y*car.width + x*8 + (7-i)] = (bitmap[idx] >> i) & 1;
+                    }
+                    idx++;
+                }
+                idx += (car.width/4) - (car.width/8);
+            }
+            delete []bitmap;
+        }
+        else {
+            car.width = gm.gmBlackBoxX;
+            car.bitmap = new uint8_t[car.width*car.height];
+            std::memset(car.bitmap, 0, car.width*car.height);
+        }
+        car.leftbearing =  gm.gmptGlyphOrigin.x;
+        car.xAdvance = gm.gmCellIncX; // + car.leftbearing;
+        car.ascent = gm.gmptGlyphOrigin.y;
+        car.descent = gm.gmBlackBoxY-gm.gmptGlyphOrigin.y;
+        ReleaseDC(nullptr, dc);
+    }
+
+#endif
+
 
 
 }
