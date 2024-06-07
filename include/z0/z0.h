d77ae06b13595be989b2e12a557b0806aff41d09
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-06-07 11:40:30 +0200

    moving raycast to work-in-progress

--- include/z0/z0.h
+++ include/z0/z0.h
@@ -46,7 +46,7 @@
 #include <z0/nodes/static_body.h>
 #include <z0/nodes/character.h>
 #include <z0/nodes/skybox.h>
-#include <z0/nodes/raycast.h>
+//#include <z0/nodes/raycast.h>
 
 #include <z0/renderers/base_renderpass.h>
 #include <z0/renderers/skybox_renderer.h>
