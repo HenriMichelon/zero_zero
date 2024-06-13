9763fd08caee28115c0010ce17c1313c8425d072
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-06-13 11:30:47 +0200

    RTD Sphynx config

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,28 @@
-# -- Project information
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ZeroZero'
 copyright = '2024, Henri Michelon'
 author = 'Henri Michelon'
-
 release = '0.0'
-version = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
