cf3221d9fd9c1f499a61f8589ee10f345fa19022
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-06-18 16:30:54 +0200

    documenting  Signal

--- include/z0/signal.h
+++ include/z0/signal.h
@@ -4,13 +4,40 @@ namespace z0 {
 
     class Object;
 
+    /**
+     * A signal of an Object
+     */
     class Signal {
     public:
+        /**
+         * Base struct for emit() parameters
+         */
         struct Parameters {};
+
+        /**
+         * Callable member function
+         */
         typedef void (Object::*Handler)(Parameters*);
         
+        /**
+         * Connects a member function to the signal
+         * @param object object containing the member function to connect
+         * @param handler the member function to call when emit() is called
+        */
         void connect(Object* object, Handler handler);
+
+        /**
+         * Disconnects a member function to the signal
+         * @param object object containing the member function to connect
+         * @param handler the member function to call when emit() is called
+        */
         void disconnect(Object* object, Handler handler);
+
+        /**
+         * Emits the signal by calling all the connected functions in the connect order
+         * @param name signal name
+         * @param params parameters to pass to the function connected to the signal
+         */
         void emit(Parameters* param);
 
     private:
