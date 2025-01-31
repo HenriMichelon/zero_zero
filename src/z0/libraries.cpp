fe2c4305255cb41269583cfe053db7fafe65d6bb
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-01-31 09:00:00 +0100

    Updated VMA

--- src/z0/libraries.cpp
+++ src/z0/libraries.cpp
@@ -9,7 +9,7 @@
 #endif
 #include "z0/vulkan.h"
 #define VMA_IMPLEMENTATION
-#define VMA_VULKAN_VERSION 1003000
+#define VMA_VULKAN_VERSION 1004000
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
 #include <vk_mem_alloc.h>
