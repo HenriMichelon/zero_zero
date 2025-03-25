64f4db9dcf377a4652bffd164d25916d889aabc4
Henri <148531229+HenriMichelon@users.noreply.github.com>
2025-03-25 15:12:01 +0100

    Reactivate PCF for shadows

--- src/z0/shaders/celshading.frag
+++ src/z0/shaders/celshading.frag
@@ -7,7 +7,6 @@
 #version 450
 
 #include "utils.glsl"
-#include "shadows/shadows.glsl"
 
 layout (location = 0) in VertexOut fs_in;
 layout (location = 0) out vec4 COLOR;
@@ -30,6 +29,69 @@ vec3 calcPointLight(Light light, vec3 normal, vec3 viewDirection, vec3 fragPos) 
     } else {
         return min(diffuse, vec3(1.0f));
     }
+}
+
+
+#define SHADOW_FACTOR 0.2f
+
+float shadowFactor(Light light, int cascadeIndex, vec4 fragPos) {
+    // https://learnopengl.com/Advanced-Lighting/Shadows/Shadow-Mapping
+    const vec3 texelSize = 1.0 / textureSize(shadowMaps[light.mapIndex], 0);
+    const vec4 ShadowCoord = light.lightSpace[cascadeIndex] * fragPos;
+    vec3 projCoords = ShadowCoord.xyz / ShadowCoord.w;
+    if (projCoords.z > 1.0) return 1.0f;
+    projCoords.xy = projCoords.xy * 0.5 + 0.5;
+    const bool outOfView = (projCoords.x < 0.001f || projCoords.x > 0.999f || projCoords.y < 0.001f || projCoords.y > 0.999f);
+    if (outOfView) return 1.0f;
+    const float closestDepth = texture(shadowMaps[light.mapIndex], vec3(projCoords.xy, float(cascadeIndex))).r;
+    const float currentDepth = projCoords.z;
+    //    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
+    float bias = 0.005;
+    return currentDepth - bias > closestDepth ? SHADOW_FACTOR : 1.0;
+
+    // PCF
+    //    const float bias   = 0.0005;
+    //    float shadow       = 0.0;
+    //    for(int x = -1; x <= 1; ++x)  {
+    //        for(int y = -1; y <= 1; ++y) {
+    //            float pcfDepth = texture(shadowMaps[light.mapIndex], vec3(projCoords.xy + vec2(x, y) * texelSize.xy, float(cascadeIndex))).r;
+    //            shadow += (currentDepth - bias) > pcfDepth ? SHADOW_FACTOR : 1.0;
+    //        }
+    //    }
+    //    return shadow /= 9.0;
+}
+
+//const vec3 sampleOffsetDirections[20] = vec3[] (
+//    vec3( 1,  1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1,  1,  1),
+//    vec3( 1,  1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1,  1, -1),
+//    vec3( 1,  1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1,  1,  0),
+//    vec3( 1,  0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1,  0, -1),
+//    vec3( 0,  1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0,  1, -1)
+//);
+
+float shadowFactorCubemap(Light light, vec3 fragPos) {
+    // https://learnopengl.com/Advanced-Lighting/Shadows/Point-Shadows
+    const float bias         = 0.15;
+    //    const int   samples      = 20;
+    //    const float viewDistance = length(global.cameraPosition - fragPos);
+    //    const float diskRadius   = (1.0 + (viewDistance / light.farPlane)) / 25.0;
+
+    // get vector between fragment position and light position
+    const vec3 fragToLight = fragPos - light.position;
+    const float currentDepth = length(fragToLight);
+    const float closestDepth = texture(shadowMapsCubemap[light.mapIndex], fragToLight).r * light.farPlane;
+    return currentDepth - bias > closestDepth ? SHADOW_FACTOR : 1.0;
+
+    // PCF
+    //    float shadow = 0.0;
+    //    for(int i = 0; i < samples; ++i) {
+    // use the light to fragment vector to sample from the depth map
+    // it is currently in linear range between [0,1]. Re-transform back to original value
+    //        shadow += currentDepth >
+    //                texture(shadowMapsCubemap[light.mapIndex], fragToLight + sampleOffsetDirections[i] * diskRadius).r * light.farPlane
+    //                ? SHADOW_FACTOR : 1.0;
+    //    }
+    //    return (shadow /= samples);
 }
 
 void main() {
--- src/z0/shaders/shadows/shadows.glsl
+++ src/z0/shaders/shadows/shadows.glsl
@@ -19,50 +19,50 @@ float shadowFactor(Light light, int cascadeIndex, vec4 fragPos) {
     const float closestDepth = texture(shadowMaps[light.mapIndex], vec3(projCoords.xy, float(cascadeIndex))).r;
     const float currentDepth = projCoords.z;
 //    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
-    float bias = 0.005;
-    return currentDepth - bias > closestDepth ? SHADOW_FACTOR : 1.0;
+//    float bias = 0.005;
+//    return currentDepth - bias > closestDepth ? SHADOW_FACTOR : 1.0;
 
     // PCF
-//    const float bias   = 0.0005;
-//    float shadow       = 0.0;
-//    for(int x = -1; x <= 1; ++x)  {
-//        for(int y = -1; y <= 1; ++y) {
-//            float pcfDepth = texture(shadowMaps[light.mapIndex], vec3(projCoords.xy + vec2(x, y) * texelSize.xy, float(cascadeIndex))).r;
-//            shadow += (currentDepth - bias) > pcfDepth ? SHADOW_FACTOR : 1.0;
-//        }
-//    }
-//    return shadow /= 9.0;
+    const float bias   = 0.0005;
+    float shadow       = 0.0;
+    for(int x = -1; x <= 1; ++x)  {
+        for(int y = -1; y <= 1; ++y) {
+            float pcfDepth = texture(shadowMaps[light.mapIndex], vec3(projCoords.xy + vec2(x, y) * texelSize.xy, float(cascadeIndex))).r;
+            shadow += (currentDepth - bias) > pcfDepth ? SHADOW_FACTOR : 1.0;
+        }
+    }
+    return shadow /= 9.0;
 }
 
-//const vec3 sampleOffsetDirections[20] = vec3[] (
-//    vec3( 1,  1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1,  1,  1),
-//    vec3( 1,  1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1,  1, -1),
-//    vec3( 1,  1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1,  1,  0),
-//    vec3( 1,  0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1,  0, -1),
-//    vec3( 0,  1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0,  1, -1)
-//);
+const vec3 sampleOffsetDirections[20] = vec3[] (
+    vec3( 1,  1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1,  1,  1),
+    vec3( 1,  1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1,  1, -1),
+    vec3( 1,  1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1,  1,  0),
+    vec3( 1,  0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1,  0, -1),
+    vec3( 0,  1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0,  1, -1)
+);
 
 float shadowFactorCubemap(Light light, vec3 fragPos) {
     // https://learnopengl.com/Advanced-Lighting/Shadows/Point-Shadows
     const float bias         = 0.15;
-//    const int   samples      = 20;
-//    const float viewDistance = length(global.cameraPosition - fragPos);
-//    const float diskRadius   = (1.0 + (viewDistance / light.farPlane)) / 25.0;
+    const int   samples      = 20;
+    const float viewDistance = length(global.cameraPosition - fragPos);
+    const float diskRadius   = (1.0 + (viewDistance / light.farPlane)) / 25.0;
 
     // get vector between fragment position and light position
     const vec3 fragToLight = fragPos - light.position;
     const float currentDepth = length(fragToLight);
-    const float closestDepth = texture(shadowMapsCubemap[light.mapIndex], fragToLight).r * light.farPlane;
-    return currentDepth - bias > closestDepth ? SHADOW_FACTOR : 1.0;
+//    const float closestDepth = texture(shadowMapsCubemap[light.mapIndex], fragToLight).r * light.farPlane;
+//    return currentDepth - bias > closestDepth ? SHADOW_FACTOR : 1.0;
 
     // PCF
-//    float shadow = 0.0;
-//    for(int i = 0; i < samples; ++i) {
-        // use the light to fragment vector to sample from the depth map
-        // it is currently in linear range between [0,1]. Re-transform back to original value
-//        shadow += currentDepth >
-//                texture(shadowMapsCubemap[light.mapIndex], fragToLight + sampleOffsetDirections[i] * diskRadius).r * light.farPlane
-//                ? SHADOW_FACTOR : 1.0;
-//    }
-//    return (shadow /= samples);
+    float shadow = 0.0;
+    for(int i = 0; i < samples; ++i) {
+//         use the light to fragment vector to sample from the depth map
+//         it is currently in linear range between [0,1]. Re-transform back to original value
+        shadow += currentDepth >
+                texture(shadowMapsCubemap[light.mapIndex], fragToLight + sampleOffsetDirections[i] * diskRadius).r * light.farPlane
+                ? SHADOW_FACTOR : 1.0;
+    }
+    return (shadow /= samples);
 }
